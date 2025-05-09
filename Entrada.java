3ce184c1723a7472dd73d1740407a2f64bd977c1
LeandroDem21 <20243rd051@utez.edu.mx>
2025-05-09 09:37:20 -0600

    Este codigo muestra tu nombre completo

--- Entrada.java
+++ Entrada.java
@@ -1,6 +1,9 @@
 import javax.swing.JOptionPane;
 public class Entrada{
     public static void main(String[]args){
-        String nombreUsuario= JOptionPane.showInputDialog();
+        String nombreUsuario= JOptionPane.showInputDialog("Ingresa tu nombre");
+        String apellidoUsuario= JOptionPane.showInputDialog("Ingresa tu primer apellido");
+        String apellidoUsuario2= JOptionPane.showInputDialog("Ingresa tu segundo apellido");
+        System.out.println("Tu nombre completo es:" +nombreUsuario+ " " +apellidoUsuario+ " " +apellidoUsuario2);
     }
 }
